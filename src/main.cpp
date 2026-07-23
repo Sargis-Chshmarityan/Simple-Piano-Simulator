1f4c946cd4d6fd95aa5539e58c43e72fd307c942
Sargis <sargistshshmarityan@gmail.com>
2026-07-23 19:26:15 +0400

    Cleaned up the file, moved everything to app.hpp and audio_handler.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
-#include "piano.hpp"
-#include <SDL2/SDL.h>
+#include "app.hpp"
 
-int main() {
-  Piano piano;
-  piano.play(1);
-  piano.play(5);
-  piano.play(8);
-  return 0;
+void start(int argc, char *argv[]) {
+  std::cout << "Running main with args:" << ((argc == 1) ? " - " : "");
+  for (int i = 1; i < argc; i++) {
+    std::cout << argv[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  start(argc, argv);
+  App app;
+  return app.run();
 }
